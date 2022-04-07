00c2a8435b730e66cb0c71e9388d728071e1d36e
apc9041 <apc9041@gmail.com>
2022-04-07 19:19:34 +0700

    update Day1 and Day3

---
+++ day2_html/workshop/Day3/HomeWork/hw1.4.js
@@ -0,0 +1,13 @@
+function draw(n) {
+    text = '';
+    for (let i=0; i<n; i++ ) {
+        for (let j=0; j<n; j++) {
+            text += i+1;
+        }
+        text += '\n';
+    }
+    console.log(text);
+}
+draw(2)
+draw(3)
+draw(4)
