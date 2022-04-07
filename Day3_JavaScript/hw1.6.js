364dac08b1c21c781f36d954a93415ca9dfb8d0f
apc9041 <apc9041@gmail.com>
2022-04-07 21:12:42 +0700

    add day3 homework

---
+++ Day3_JavaScript/hw1.6.js
@@ -0,0 +1,14 @@
+debugger
+function draw(n) {
+    text = '';
+    let a=1;
+    for (let i=0; i<n; i++ ) {
+        for (let j=0; j<n; j++) {
+            text += a;
+            a += 1;
+        }
+        text += '\n';
+    }
+    console.log(text);
+}
+draw(2)
